f8db8f3b94b5b944855144ca413579982f8bd374
Brian Gerstle <bgerstle@lookback.io>
2018-08-22 16:03:20 -0500

    fix bootstrap to install pip & ansible globally
    ideally wouldn't need sudo, but...

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,5 +7,8 @@ xcode-select --install
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# Install pip
+sudo easy_install pip
+
 # Install ansible
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
