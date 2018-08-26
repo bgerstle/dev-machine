92af6217146c0969c11a42f2f3b744670ae9370a
Brian Gerstle <brian.gerstle@gmail.com>
2018-08-26 08:56:27 -0500

    remove xcode-select install
    homebrew installs CLI tools. this might cause problems w/ Macvim which
    requires xcode

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# Install Command-Line Tools
-# NOTE: requires user interaction
-xcode-select --install
-
 # Install Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
